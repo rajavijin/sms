9783b1c92b83b86217173247c65e31fb5ba7911e
sahaya rex <rexdeveloper@gmail.com>
2015-11-22 19:09:29 +0530

    Clean up SMS

--- scripts/angularfire/config.js
+++
@@ -1,6 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://inditesms.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password'])
-
-  .constant('loginRedirectPath', '/login')
-  .constant('loggedInPath', '/compose');
--- scripts/filters/reverse.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('inditesmsApp')
-  .filter('reverse', function() {
-    return function(items) {
-      return angular.isArray(items)? items.slice().reverse() : [];
-    };
-  });
